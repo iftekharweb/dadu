83362bae76b3b3cdae69f73c58f9e9a81ac6297c
iftekharweb <iftekharweb@gmail.com>
2024-03-30 12:41:30 +0600

    start dashboard

--- frontend_main/src/App.jsx
+++ frontend_main/src/App.jsx
@@ -4,11 +4,8 @@ import Login from './components/Login';
 import Profile from './components/Profile';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import ResetPassword from './components/ResetPassword';
-<<<<<<< HEAD
 import Home from './dashboard/Home';
-=======
 import FramePage from './pages/Frame';
->>>>>>> 17a1a6134571117e7619d49d0bd6ef4475dded3b
 
 function App() {
     const [redirectToProfile, setRedirectToProfile] = useState(false);
@@ -19,11 +16,8 @@ function App() {
                 <Route path='/auth/login' element={<Login/>}></Route>
                 <Route path='/profile' index element={<Profile/>}></Route>
                 <Route path='/auth/reset-password' index element={<ResetPassword/>}></Route>
-<<<<<<< HEAD
                 <Route path='/dashboard' index element={<Home/>}></Route>
-=======
                 <Route path='/dashboard' index element={<FramePage/>}></Route>
->>>>>>> 17a1a6134571117e7619d49d0bd6ef4475dded3b
                 <Route path='/*' element={<Profile />} />
             </Routes>
         </BrowserRouter>
